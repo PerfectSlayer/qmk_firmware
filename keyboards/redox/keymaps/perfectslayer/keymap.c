022d65bb1df8240cf49b5adf5868f1b5437916cf
Bruce Bujon <bruce.bujon@datadoghq.com>
2023-08-23 08:25:20 +0200

    feat: Improve navigation arrows

--- keyboards/redox/keymaps/perfectslayer/keymap.c
+++ keyboards/redox/keymaps/perfectslayer/keymap.c
@@ -28,19 +28,24 @@ enum custom_keycodes {
 #define KC_ADEN LT(JUST, KC_END)
 #define KC_ADPU LT(_ADJUST, KC_PGUP)
 
+// Custom Key: Right Alt when held, Toggle RGB when tapped
+#define CK_RALT RALT_T(RGB_TOG)
+// Custom Key: Right Control when held, ç when tapped
+#define CK_RCTL RCTL_T(KC_NUHS)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_ESC  ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,                                            KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,KC_EQL  ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,XXXXXXX ,                          KC_NUHS ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_LBRC ,
+     KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,XXXXXXX ,                          XXXXXXX ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_LBRC ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LCTL ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,KC_GRV  ,                          KC_MINS  ,KC_H    ,KC_J    ,KC_K    ,KC_L   ,KC_SCLN ,KC_QUOTE,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_DEL  ,KC_HOME ,        KC_PGUP ,RGB_TOG ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_RBRC ,
+     KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_DEL  ,KC_HOME ,        KC_PGUP ,CK_RALT ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_RBRC ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
-     KC_LCTL ,KC_LWIN ,SYM_L   ,KC_LALT ,     KC_SPC  ,    KC_BSPC ,KC_END  ,        KC_PGDN ,KC_ENT  ,    KC_RALT ,     KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RGHT 
+     KC_LCTL ,KC_LWIN ,SYM_L   ,KC_LALT ,     KC_SPC  ,    KC_BSPC ,KC_END  ,        KC_PGDN ,KC_ENT  ,    KC_LEFT ,     KC_DOWN ,KC_UP   ,KC_RGHT ,CK_RCTL 
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
   ),
 
