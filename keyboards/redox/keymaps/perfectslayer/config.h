e9f16819c8dbe142c5ea79cdd8cad549332f11c9
PerfectSlayer <bruce.bujon@gmail.com>
2024-07-14 19:25:46 +0200

    feat: Use EEPROM handedness

--- keyboards/redox/keymaps/perfectslayer/config.h
+++ keyboards/redox/keymaps/perfectslayer/config.h
@@ -19,8 +19,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Select hand configuration */
 // #define MASTER_LEFT
-#define MASTER_RIGHT
-// #define EE_HANDS
+// #define MASTER_RIGHT
+#define EE_HANDS
 
 #undef RGBLIGHT_LED_COUNT
 #define RGBLIGHT_EFFECT_BREATHING
