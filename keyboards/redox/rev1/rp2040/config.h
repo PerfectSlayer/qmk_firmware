fef5cbab04d6ebe5dc3ea4be3f615ad81b720baa
PerfectSlayer <bruce.bujon@gmail.com>
2024-08-10 11:35:53 +0200

    feat: Add OLED support

---
+++ keyboards/redox/rev1/rp2040/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#ifdef OLED_ENABLE
+#define I2C1_SDA_PIN GP14
+#define I2C1_SCL_PIN GP15
+#endif
--- keyboards/redox/rev1/rp2040/rules.mk
+++ keyboards/redox/rev1/rp2040/rules.mk
@@ -1,2 +1,6 @@
-# specific to using the serial serial driver
+# specific to using the serial driver
 SERIAL_DRIVER = vendor
+# specific to using the OLED driver
+OLED_ENABLE = yes
+OLED_DRIVER = ssd1306
+OLED_TRANSPORT = i2c
