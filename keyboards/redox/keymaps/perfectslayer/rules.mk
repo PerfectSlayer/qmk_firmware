942e4996980d0d08db91e71628374d0f174a47f0
PerfectSlayer <bruce.bujon@gmail.com>
2024-09-05 19:30:40 +0200

    feat: Add basic OLED features and animations prototype

--- keyboards/redox/keymaps/perfectslayer/rules.mk
+++ keyboards/redox/keymaps/perfectslayer/rules.mk
@@ -1,3 +1,8 @@
+# Add OLED features
+SRC += oled.c
+# Enable VIA support
 VIA_ENABLE = yes
 # Enable LTO for smaller firmware on the original pro micro
 #LTO_ENABLE = yes
+# Enable debug mode
+CONSOLE_ENABLE = yes
