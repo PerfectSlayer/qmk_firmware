2401147affaf6eeb3d7fd4cbcd4236e2345cdf0f
PerfectSlayer <bruce.bujon@gmail.com>
2026-01-17 14:55:40 +0100

    feat: Refine keymap

--- keyboards/splitkb/aurora/lily58/keymaps/perfectslayer/keymap.c
+++ keyboards/splitkb/aurora/lily58/keymaps/perfectslayer/keymap.c
@@ -5,13 +5,17 @@
 #define ADJUS_L MO(_ADJUST)
 
 #define KC_LOSP LT(_LOWER, KC_SPC)
+#define KC_RAQU RALT_T(KC_QUOT) // Right Alt when held, M when tapped (Quote as M when remapped)
+
+// Left Shift and left control need improvements
+// On the right part, backspace and right GUI can be removed. What put instead?
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
-        KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                   KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_GRV ,
-        KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                   KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_MINS,
-        KC_LSFT, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                   KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
-        KC_LCTL, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_LBRC, KC_RBRC, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
+        KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                   KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_Z   ,
+        KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                   KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC,
+        KC_LSFT, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                   KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_RAQU,
+        KC_LCTL, KC_LSFT, KC_X   , KC_C   , KC_V   , KC_B   , KC_GRV , KC_MINS, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RBRC,
                                    KC_LGUI, KC_LALT, KC_LOSP, KC_BSPC, KC_ENT , RAISE_L, KC_BSPC, KC_RGUI
     ),
     [_LOWER] = LAYOUT(
@@ -48,10 +52,10 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     } else {
         // Default behavior for all other layers
         if (clockwise) {
-            // Redo: Ctrl + Shift + Z
+            // Redo: Ctrl + Shift + Z (left bracket used as Z when remapped)
             tap_code16(C(S(KC_LBRC)));
         } else {
-            // Undo: Ctrl + Z
+            // Undo: Ctrl + Z (left bracket used as Z when remapped)
             tap_code16(C(KC_LBRC));
         }
     }
