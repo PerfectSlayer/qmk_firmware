93314110f9aa04b0d7bbd7148ea7a607ea29ab6f
PerfectSlayer <bruce.bujon@gmail.com>
2026-04-04 09:45:36 +0200

    feat: Improve layout
    Improve ctlt and shift
    Improve tab / raise like space / lower
    Add % key
    Add à key

--- keyboards/splitkb/aurora/lily58/keymaps/perfectslayer/keymap.c
+++ keyboards/splitkb/aurora/lily58/keymaps/perfectslayer/keymap.c
@@ -8,19 +8,20 @@
 #define RAISE_L MO(_RAISE)
 #define ADJUS_L MO(_ADJUST)
 
-#define KC_LOSP LT(_LOWER, KC_SPC)
+#define KC_LOSP LT(_LOWER, KC_SPC) // Turn on Lower when held, Space when tapped
+#define KC_RSTB LT(_RAISE, KC_TAB) // Turn on Raise when held, Tab when tapped
 #define KC_RAQU RALT_T(KC_QUOT) // Right Alt when held, M when tapped (Quote as M when remapped)
 
-// Left Shift and left control need improvements
+// Left Shift and left control need improvements (LSFT is doubled)
 // On the right part, backspace and right GUI can be removed. What put instead?
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
-        KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                   KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_Z   ,
+        KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                   KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_EQL ,
         KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                   KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC,
-        KC_LSFT, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                   KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_RAQU,
-        KC_LCTL, KC_LSFT, KC_X   , KC_C   , KC_V   , KC_B   , KC_GRV , KC_MINS, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RBRC,
-                                   KC_LGUI, KC_LALT, KC_LOSP, KC_BSPC, KC_ENT , RAISE_L, KC_BSPC, KC_RGUI
+        KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                   KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_RAQU,
+        KC_LSFT, KC_LSFT, KC_X   , KC_C   , KC_V   , KC_B   , KC_GRV , KC_MINS, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RBRC,
+                                   KC_LGUI, KC_LALT, KC_LOSP, KC_BSPC, KC_ENT , KC_RSTB, KC_BSPC, KC_RGUI
     ),
     [_LOWER] = LAYOUT(
         _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
@@ -32,8 +33,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_RAISE] = LAYOUT(
         _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
         KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                   KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , _______,
-        KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  ,                   XXXXXXX, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, XXXXXXX,
-        KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______, _______, KC_PLUS, KC_MINS, KC_EQL , KC_LBRC, KC_RBRC, KC_BSLS,
+        _______, KC_Z   , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, XXXXXXX,
+        _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, KC_PLUS, KC_MINS, KC_EQL , KC_LBRC, KC_RBRC, KC_BSLS,
                                    _______, _______, _______, KC_DEL , _______, _______, _______, _______
     ),
     [_ADJUST] = LAYOUT(
